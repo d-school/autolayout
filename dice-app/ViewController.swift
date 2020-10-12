14baa891fa94f6c3d7a1197a507cc46382e64a0e
julesug@gmail.com <julesug@gmail.com>
2020-10-12 21:33:17 +0900

    add images

--- dice-app/ViewController.swift
+++ dice-app/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    var diceImages = [#imageLiteral(resourceName: "one"), #imageLiteral(resourceName: "two"), #imageLiteral(resourceName: "three"), #imageLiteral(resourceName: "four"), #imageLiteral(resourceName: "five"), #imageLiteral(resourceName: "six")]
+    
+    
+    @IBAction func rollButton(_ sender: Any) {
+        
+        diceImage1.image = diceImages[Int.random(in: 0...5)]
+        
+        
     }
-
+    
 
 }
 
